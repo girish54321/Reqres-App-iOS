416b166f692d68cbdf1fc6350e764229df3640d8
Girish Parate <girishparate41@gmail.com>
2022-02-26 23:18:46 +0530

    Update api call & code claen up

--- Reqres App/ListItems/UserListItemCell.swift
+++ Reqres App/ListItems/UserListItemCell.swift
@@ -20,7 +20,6 @@ class UserListItemCell: UITableViewCell {
     
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
         // Configure the view for the selected state
     }
     
